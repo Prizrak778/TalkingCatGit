bcabe09f69f2912e7152ab7a80ee14459c69c42f
prizrak778 <v.merkel778@gmail.com>
2019-10-23 00:27:21 +0700

    Добавлена модель для диалогов между пользователями

---
+++ TalkingCatGit/Models/CatDialogue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalkingCat.Models
+{
+    public class CatDialogue
+    {
+        //Id Dialogue
+        public int Id { get; set; }
+
+        //Id User1
+        public int? User1Id { get; set; }
+        public AllUserCat User1 { get; set; }
+
+        //Id User2
+        public int? User2Id { get; set; }
+        public AllUserCat User2 { get; set; }
+
+        //Id Last Message
+        public int? LastMessageId { get; set; }
+
+
+    }
+}

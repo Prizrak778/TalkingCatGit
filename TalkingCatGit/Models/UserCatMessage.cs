9a1d4f75f913c2c213c1df19b1adab6537371c21
prizrak778 <v.merkel778@gmail.com>
2019-10-23 00:27:49 +0700

    Добавлена модель для сообщений пользователей

---
+++ TalkingCatGit/Models/UserCatMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalkingCat.Models
+{
+    public class UserCatMessage
+    {
+        //Id Message
+        public int Id { get; set; }
+
+        //Message
+        public string Message { get; set; }
+
+        //Date Message
+        public DateTime DateMessage { get; set; }
+
+        public int DialogId { get; set; }
+
+        //Id Author Message
+        public int AuthorMessageId { get; set; }
+    }
+}

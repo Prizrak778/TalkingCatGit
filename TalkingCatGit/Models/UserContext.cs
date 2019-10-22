0d5a1c24e2faf1318b6372b01f60acf4bd7ee6bf
prizrak778 <v.merkel778@gmail.com>
2019-10-23 00:28:43 +0700

    Добавлено контекст данных

---
+++ TalkingCatGit/Models/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TalkingCat.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<AllUserCat> UserCats { get; set; }
+        public DbSet<CatDialogue> catDialogues { get; set; }
+        public DbSet<UserCatMessage> userCatMessages { get; set; }
+
+    }
+}

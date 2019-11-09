1a7fa9a53e9ddccca2b338184c44d3002aedce20
prizrak778 <v.merkel778@gmail.com>
2019-11-10 02:05:14 +0700

    Добавлена связь между сообщением и автором сообщения

--- TalkingCatGit/SQL_Scripts/dbo.UserCatMessages.sql
+++ TalkingCatGit/SQL_Scripts/dbo.UserCatMessages.sql
@@ -4,6 +4,12 @@
     [DateMessage]     DATETIME       NOT NULL,
     [DialogId]        INT            NOT NULL,
     [AuthorMessageId] INT            NOT NULL,
-    CONSTRAINT [PK_dbo.UserCatMessages] PRIMARY KEY CLUSTERED ([Id] ASC)
+    CONSTRAINT [PK_dbo.UserCatMessages] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_dbo.UserCatMessages_dbo.AllUserCats_AuthorMessageId] FOREIGN KEY ([AuthorMessageId]) REFERENCES [dbo].[AllUserCats] ([Id]) ON DELETE CASCADE
 );
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_AuthorMessageId]
+    ON [dbo].[UserCatMessages]([AuthorMessageId] ASC);
 

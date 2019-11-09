54072a20f98726b19ffdc7a4b718727caea3de68
prizrak778 <v.merkel778@gmail.com>
2019-11-10 02:06:00 +0700

    Добавлены модели для картинок и альбомов

---
+++ TalkingCatGit/Models/CatAlbum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalkingCat.Models
+{
+    public class CatAlbum
+    {
+        //Id Img
+        public int Id { get; set; }
+
+        //Name
+        public string NameAlbum { get; set; }
+
+        //Imgs
+        public virtual ICollection<CatImg> CatImgs { get; set; }
+        public virtual ICollection<AllUserCat> CatUsers { get; set; }
+
+        public CatAlbum()
+        {
+            CatUsers = new List<AllUserCat>();
+            CatImgs = new List<CatImg>();
+        }
+    }
+}
---
+++ TalkingCatGit/Models/CatImg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TalkingCat.Models
+{
+    public class CatImg
+    {
+        //Id Img
+        public int Id { get; set; }
+
+        //Id Author
+        public int UserId { get; set; }
+
+        public AllUserCat User { get; set; }
+
+        //Data download img
+        public DateTime DateDownload { get; set; }
+
+        //Path img
+        public string PathImg { get; set; }
+
+        //Albums
+
+        public virtual ICollection<CatAlbum> CatAlbums { get; set; }
+        public CatImg()
+        {
+            CatAlbums = new List<CatAlbum>();
+        }
+    }
+}

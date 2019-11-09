9cfea7de1c49290f8e2d80c8a8c79ef4212707ae
prizrak778 <v.merkel778@gmail.com>
2019-11-10 02:07:33 +0700

    Добавлена инит для альбома и картинок

--- TalkingCatGit/Models/UserContext.cs
+++ TalkingCatGit/Models/UserContext.cs
@@ -12,5 +12,22 @@ namespace TalkingCat.Models
         public DbSet<CatDialogue> catDialogues { get; set; }
         public DbSet<UserCatMessage> userCatMessages { get; set; }
 
+        public DbSet<CatAlbum> CatAlbums { get; set; }
+
+        public DbSet<CatImg> CatImgs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatAlbum>().HasMany(c => c.CatUsers)
+                .WithMany(s => s.CatAlbums)
+                .Map(t => t.MapLeftKey("CatAlbum_Id")
+                .MapRightKey("CatUser_Id")
+                .ToTable("CatAlbumAllUserCat"));
+            modelBuilder.Entity<CatAlbum>().HasMany(c => c.CatImgs)
+                .WithMany(s => s.CatAlbums)
+                .Map(t => t.MapLeftKey("CatImg_Id")
+                .MapRightKey("CatAlbum_Id")
+                .ToTable("CatImgCatAlbums"));
+        }
     }
 }

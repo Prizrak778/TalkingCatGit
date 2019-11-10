0637ca1e8f60f30da2e5f3f7e000a32afb73d7fc
prizrak778 <v.merkel778@gmail.com>
2019-11-10 17:34:31 +0700

    Функция инициализации в Global.aspx

--- TalkingCatGit/Global.asax.cs
+++ TalkingCatGit/Global.asax.cs
@@ -15,6 +15,8 @@ namespace TalkingCatGit
     {
         protected void Application_Start()
         {
+			Database.SetInitializer(new UserDbInitializer());
+			
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
